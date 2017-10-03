59ad8525a8337392662b4a7d10593fbe3ff2d222
manisbindra <maninder.bindra@gmail.com>
2017-10-03 21:12:58 +0530

    added persistent volume using azure files

--- canary-spinnaker/using-spinnaker-helm-chart-and-existing-cluster/installation.sh
+++ canary-spinnaker/using-spinnaker-helm-chart-and-existing-cluster/installation.sh
@@ -1,6 +1,7 @@
+
 # provision the k8s cluster with custom subnet. Starting from current directory as working directory
-cd ../../
-./provision.sh
+# cd ../../
+# ./provision.sh
 
 
 # From portal associate routing table to subnet 1
@@ -9,13 +10,34 @@ cd ../../
 cd ./canary-spinnaker/using-spinnaker-helm-chart-and-existing-cluster/
 
 # install eureka service and eureka service client using helm chart
-helm install --name eureka-services ../../eur-helm
+# helm install --name eureka-services ../../eur-helm
 
+
+# create Persistent volume
+
+# create group
+az group create -n acspvresgrp -l westcentralus
+
+# create storage account
+az storage account create -n pvstoaccpoc -l westcentralus -g acspvresgrp --sku Standard_LRS
+
+
+# create persistent volume
+export current_env_conn_string=$(az storage account show-connection-string -n pvstoaccpoc -g acspvresgrp --query 'connectionString' -o tsv)
+# create secret
+az storage share create --name pvfileshare --quota 2048 --connection-string $current_env_conn_string
+kubectl apply -f azure-secret.yaml
+kubectl apply -f persistent-volume.yaml
+
+
+# add pvclaim
+kubectl apply -f minio-pv-claim.yaml
 
 # Values file in config directory was copied from the spinnaker helm github repo : https://raw.githubusercontent.com/kubernetes/charts/master/stable/spinnaker/values.yaml . The only modification made to the file is that minio is disabled . Spinnaker chart repo : https://github.com/kubernetes/charts/tree/master/stable/spinnaker
 
+
 # install spinnaker using chart
-helm install stable/spinnaker --name spinnakerinst -f ./config/values.yaml --timeout 900
+helm install stable/spinnaker --name instspinnaker -f ./config/values.yaml --timeout 900
 
 # create spinnaker port forwarding tunnels 
 export DECK_POD=$(kubectl get pods --namespace default -l "component=deck,app=spinnakerinst-spinnaker" -o jsonpath="{.items[0].metadata.name}")
