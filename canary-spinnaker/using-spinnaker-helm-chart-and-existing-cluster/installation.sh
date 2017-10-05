6fbeab7edc1be1ffa5f74eb152e0229a17f632a7
manisbindra <maninder.bindra@gmail.com>
2017-10-05 14:17:24 +0530

    modification to default spinnaker template

--- canary-spinnaker/using-spinnaker-helm-chart-and-existing-cluster/installation.sh
+++ canary-spinnaker/using-spinnaker-helm-chart-and-existing-cluster/installation.sh
@@ -34,11 +34,32 @@ kubectl apply -f persistent-volume.yaml
 # add pvclaim
 kubectl apply -f minio-pv-claim.yaml
 
+
 # Values file in config directory was copied from the spinnaker helm github repo : https://raw.githubusercontent.com/kubernetes/charts/master/stable/spinnaker/values.yaml . The only modification made to the file is that minio is disabled . Spinnaker chart repo : https://github.com/kubernetes/charts/tree/master/stable/spinnaker
 
+# install spinnaker using chart , This gives an error
+# helm install stable/spinnaker --name instspinnaker -f ./config/values.yaml --timeout 900
 
-# install spinnaker using chart
-helm install stable/spinnaker --name instspinnaker -f ./config/values.yaml --timeout 900
+
+
+# CLONE and Modify the stable spinnaker template
+# you can clone this in a separate folder
+git clone  https://github.com/kubernetes/charts.git 
+
+# modify the file stable/spinnaker/templates/hooks/create-bucket.yaml
+# remove " &&  mc mb {{.Release.Name}}-minio/spinnaker"
+# original file line
+#         - "mc config host add {{.Release.Name}}-minio http://{{.Release.Name}}-minio-svc:9000 {{ .Values.minio.accessKey }} {{ .Values.minio.secretKey }} S3v4 &&  mc mb {{.Release.Name}}-minio/spinnaker" 
+# has been replaced by
+# modified file line
+#        - "mc config host add {{.Release.Name}}-minio http://{{.Release.Name}}-minio-svc:9000 {{ .Values.minio.accessKey }} {{ .Values.minio.secretKey }} S3v4 "
+sed -i -e 's/ &&  mc mb {{.Release.Name}}-minio\/spinnaker//g'  ./stable/spinnaker/templates/hooks/create-bucket.yaml
+
+# build helm dependencies
+helm dep build ./stable/spinnaker/
+
+# install modified spinnaker template
+helm install --name spin2-install ./stable/spinnaker
 
 # create spinnaker port forwarding tunnels 
 export DECK_POD=$(kubectl get pods --namespace default -l "component=deck,app=spinnakerinst-spinnaker" -o jsonpath="{.items[0].metadata.name}")
